1e649077e69a808ccb1044194299df99b0c19e38
edward wang <edwardwang0722@gmail.com>
2015-08-22 19:39:47 +0900

    change icon and switch between play and pause status

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+var isSupported = false;
+var playing = false;
+
+// check suport
 if (window.DeviceMotionEvent) {
+  isSupported = true;
   window.addEventListener('devicemotion', deviceMotionHandler, false);
 } else {
   alert('DeviceMotionEvent not supported.')
@@ -6,6 +11,11 @@ if (window.DeviceMotionEvent) {
   startBtn.className = startBtn.className + ' disable';
 }
 
-function deviceMotionHandler() {
+var playBtn = document.getElementById('play-btn');
+playBtn.addEventListener('click', function() {
+  playing = !playing;
+  var icon = 'img/' + (playing ? 'pause' : 'play') + '.svg';
+  playBtn.setAttribute('src', icon);
+});
 
 }
