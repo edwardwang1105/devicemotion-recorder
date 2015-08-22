9b41496e54242f8af52dc8769855a5720507fb49
edward wang <edwardwang0722@gmail.com>
2015-08-22 21:52:54 +0900

    show devicemotion log

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
 var isSupported = false;
 var playing = false;
 
-// check suport
-if (window.DeviceMotionEvent) {
-  isSupported = true;
-  window.addEventListener('devicemotion', deviceMotionHandler, false);
-} else {
-  alert('DeviceMotionEvent not supported.')
-  var startBtn = document.getElementById('start-record');
-  startBtn.className = startBtn.className + ' disable';
+init();
+
+function init() {
+  if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
+    isSupported = true;
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
+  } else {
+    alert('DeviceMotionEvent not supported.')
+    var startBtn = document.getElementById('start-record');
+    startBtn.className = startBtn.className + ' disable';
+  }
+
+  var playBtn = document.getElementById('play-btn');
+  playBtn.addEventListener('click', function() {
+    playing = !playing;
+    var icon = 'img/' + (playing ? 'pause' : 'play') + '.svg';
+    playBtn.setAttribute('src', icon);
+  });
 }
 
-var playBtn = document.getElementById('play-btn');
-playBtn.addEventListener('click', function() {
-  playing = !playing;
-  var icon = 'img/' + (playing ? 'pause' : 'play') + '.svg';
-  playBtn.setAttribute('src', icon);
-});
 
+function deviceMotionHandler(eventData) {
+  if(!isSupported || !playing) return;
+
+  var acceleration = eventData.acceleration;
+  var accelerationG = eventData.accelerationIncludingGravity;
+  var rotation = eventData.rotationRate;
+
+  document.getElementById('log').value +=
+      '[Acceleration] ' + acceleration.x + ', ' + acceleration.y + ', ' + acceleration.z + ' ' +
+      '[AccelerationIncludingG] ' + accelerationG.x + ', ' + accelerationG.y + ', ' + accelerationG.z + ' ' +
+      '[Rotation] ' + rotation.alpha + ', ' + rotation.beta + ', ' + rotation.gamma + '/n';
 }
